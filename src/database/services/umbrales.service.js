98540824d7ef224949b1825c8e281154cebe91f1
Daniel Vega Diez <dvegadiez@gmail.com>
2024-01-07 07:57:48 -0500

    feat: adicionar la posibilidad de configurar los umbrales y obtener la condifguracion actual mendiante comandos

--- src/database/services/umbrales.service.js
+++ src/database/services/umbrales.service.js
@@ -10,4 +10,24 @@ async function obtenerUmbralesPorMoneda(moneda) {
   }
 }
 
-module.exports = { obtenerUmbralesPorMoneda }; 
+async function obtenerUmbralesTodos() {
+  try {
+    return await Umbral.findAll();
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+async function actualizarUmbralesMoneda(moneda, venta, compra) {
+  try {
+   
+    return await Umbral.update({venta: isNaN(venta) ? null : venta, compra: isNaN(compra) ? null : compra, }, { where: {moneda}})
+    
+  } catch (error) {
+    console.log(error);  
+  }
+}
+
+module.exports = { obtenerUmbralesPorMoneda, actualizarUmbralesMoneda, obtenerUmbralesTodos }; 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ const { TelegramClient } = require("telegram") ;
 const { NewMessage } = require("telegram/events") ;
 const { StringSession } = require("telegram/sessions");
 const input = require("input");
-const config = require("./config");
 const express = require('express')
 const sequelize = require("./database/database");
-const { obtenerUmbralesPorMoneda } = require("./database/services/umbrales.service");
+const { obtenerUmbralesPorMoneda, actualizarUmbralesMoneda, obtenerUmbralesTodos } = require("./database/services/umbrales.service");
 
 const app = express();
 const port = process.env.PORT  ?? 8080;
@@ -33,7 +32,8 @@ sequelize.sync({alter: true});
 const apiId = Number(process.env.TELEGRAM_API_ID) ?? '';
 const apiHash = process.env.TELEGRAM_API_HASH ?? '';
 const stringSession = new StringSession(process.env.TELEGRAM_STRING_SESSION ?? ''); // fill this later with the value from session.save()
-
+const canalOfertasQvapayOficialId = 1538924093n;
+const canalOfertasPersonalizadasId = -1002123114976n;
 async function main(){
   console.log("Iniciando el cliente...");
   const client = new TelegramClient(stringSession, apiId, apiHash, {
@@ -50,9 +50,10 @@ async function main(){
   // console.log(client.session.save()); // Save this string to avoid logging in again
 
   
-  client.addEventHandler(onNewMessageRecieved, new NewMessage({chats: [1538924093n]}));  
-  
-  async function onNewMessageRecieved(event, clin) {
+  client.addEventHandler(onNewOffer, new NewMessage({chats: [canalOfertasQvapayOficialId]}));  
+  client.addEventHandler(onNewMsg, new NewMessage({chats: [canalOfertasPersonalizadasId]}));  
+
+  async function onNewOffer(event) {
     const {message: text, id: msgId} = event.message;
     // console.log(text);
   
@@ -63,12 +64,11 @@ async function main(){
     const ratio = parseFloat(text.match(ratioRegex)[1]);
     const tipoOperacion = text.match(tipoOperacionRegex)[1];
     const moneda = text.match(monedaRegex)[1];
-    const umbral = config[moneda][tipoOperacion];
   
     const umbrales = await obtenerUmbralesPorMoneda(moneda)
     const umbralPorMoneda = umbrales[String(tipoOperacion).toLowerCase()];
   
-    const esOfertaBuena = (tipoOperacion, ratio, umbral) => {
+    const esOfertaInteresante = (tipoOperacion, ratio, umbral) => {
         if (tipoOperacion === 'Venta' && umbral && ratio <= umbral) {
             return true;
         }
@@ -81,12 +81,58 @@ async function main(){
     }
   
     console.log({tipoOperacion, moneda, ratio, umbralPorMoneda});
-    console.log({ofertaInteresante: esOfertaBuena(tipoOperacion, ratio, umbralPorMoneda)});
-    if(esOfertaBuena(tipoOperacion, ratio, umbralPorMoneda)){
-        await client.forwardMessages(-1002123114976n, {fromPeer: -1001538924093n, messages: msgId });
+    console.log({ofertaInteresante: esOfertaInteresante(tipoOperacion, ratio, umbralPorMoneda)});
+
+    if(esOfertaInteresante(tipoOperacion, ratio, umbralPorMoneda)){
+        await client.forwardMessages(canalOfertasPersonalizadasId, {fromPeer: canalOfertasQvapayOficialId, messages: msgId });
   
     }
     
+  }
+
+  async function onNewMsg(event){
+    console.log(event);
+    const {message: text, id: msgId} = event.message;
+
+    const esMensajeConfiguracionValido = (msg) => {
+      // Definimos la expresión regular para validar el formato
+      const patron = /^(CUP|MLC):(null|[0-9]+(\.[0-9]+)?):(null|[0-9]+(\.[0-9]+)?)$/;
+  
+      // Verificamos si la msg coincide con el patrón
+      return patron.test(msg);
+    }
+
+    if(esMensajeConfiguracionValido(text)){
+      const [moneda, venta, compra] = text.split(':');
+
+      actualizarUmbralesMoneda(moneda, venta, compra)
+        .then((data) => {
+          if (!data) {
+            client.sendMessage(canalOfertasPersonalizadasId, {
+              message: "Error al actualizar la configuración!",
+            }); 
+            return;
+          }
+
+          client.sendMessage(canalOfertasPersonalizadasId, {
+            message: "Configuración actualizada correctamente!",
+          }); 
+        })
+        .catch((error)=> console.log(error))
+    }
+
+    if(text.startsWith("/get")){
+      obtenerUmbralesTodos()
+        .then((umbrales)=> {
+          for (const umbral of umbrales){
+            const {moneda, venta, compra } = umbral;
+            client.sendMessage(canalOfertasPersonalizadasId, {
+              message: `Moneda: ${moneda}\nVenta: ${venta}\nCompra: ${compra}`,
+            }); 
+          }
+        })
+        .catch((error)=> console.log(error))
+    }
   }
 };
 
